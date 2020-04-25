91983ffe25dd72a2ea6dabaee6ef036f7b37b6c6
silverstar194 <maloney4@wisc.edu>
2020-04-25 12:21:58 -0700

    added changes

--- app/NanoPoker/nano_poker/views.py
+++
@@ -1,8 +0,0 @@
-# chat/views.py
-from django.shortcuts import render
-
-def message(request):
-    return render(request, 'nano_poker/stats.html')
-
-def index(request):
-    return render(request, 'nano_poker/index.html')
