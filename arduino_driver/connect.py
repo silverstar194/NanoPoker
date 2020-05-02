1cceaa20980d30fbc2be92203e41ac87f8784a7b
silverstar194 <maloney4@wisc.edu>
2020-05-02 11:45:22 -0700

    added arg parser

--- arduino_driver/connect.py
+++ arduino_driver/connect.py
@@ -2,37 +2,59 @@ import serial
 import threading
 import re
 import json
+import time
+import argparse
+import sys
 
 from websocket_wrapper import WebSocket
-device_id_to_device_name = {'/dev/cu.usbserial-1450': "device_one", '/dev/cu.usbserial-1440': "device_two"}
 
-nano_poker_ws = 'ws://127.0.0.1:8001/ws/message/action'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--playerOne", help="device file path for player one", required=True)
+    parser.add_argument("--playerTwo", help="device file path for player two", required=True)
+    parser.add_argument("--pot", help="device file path for pot", required=True)
+    parser.add_argument("--nanoPokerWs", help="device file path pot", required=True)
+    args = parser.parse_args()
 
-pot_device = '/dev/cu.usbserial-1450'
-player_one_device = '/dev/cu.usbserial-1440'
-player_two_device = ''
+    player_one_device = args.playerOne
+    player_two_device =  args.playerTwo
+    pot_device = args.pot
+    nano_poker_ws = args.nanoPokerWs
 
-devices = [pot_device, player_one_device]
+    try:
+        ws = WebSocket(nano_poker_ws)
+    except:
+        print("Nano Poker backend not started at {0}".format(nano_poker_ws))
+        sys.exit()
 
-ws = WebSocket(nano_poker_ws)
+    device_id_to_device_name = {pot_device: "device_one", player_one_device: "device_two", player_one_device: "device_three"}
+    devices = [pot_device, player_one_device, player_two_device]
 
+    try:
+        for d in devices:
+            device_serial = serial.Serial(d)
+            thread = threading.Thread(target=read_from_port, args=(device_serial, d, ))
+            thread.start()
+    except:
+        print("No device {0}".format(d))
+        sys.exit()
 
-def place_bid(device_name, token_name):
-    data = {"message": {"application": "NanoPoker", "device": device_id_to_device_name[device_name], "token": token_name, "type": "bet"}}
+def place_bid(device_name, token_name, id):
+    data = {"message": {"id": id, "application": "NanoPoker", "device": device_id_to_device_name[device_name], "token": token_name, "type": "bet"}}
     ws.send(json.dumps(data))
 
 
-def cash_out(device_name, token_name):
-    data = {"message": {"application": "NanoPoker", "device": device_id_to_device_name[device_name], "token": token_name, "type": "cash_out"}}
+def cash_out(device_name, token_name, id):
+    data = {"message": {"id": id, "application": "NanoPoker", "device": device_id_to_device_name[device_name], "token": token_name, "type": "cash_out"}}
     ws.send(json.dumps(data))
 
 
-def execute_event(device, token):
+def execute_event(device, token, id):
     if device == pot_device:
-        cash_out(device, token)
+        place_bid(device, token, id)
 
     elif device in [player_one_device]:
-        place_bid(device, token)
+        cash_out(device, token, id)
 
     else:
         raise Exception("Device not registered")
@@ -41,22 +63,19 @@ def execute_event(device, token):
 def read_from_port(device, device_name):
     id = 0
     print("Reading from {0}".format(device_name))
+    last_read = 0
     while True:
         try:
             id += 1
             device_bytes = device.readline()
             decoded_bytes = device_bytes[0:len(device_bytes)].decode("utf-8")
 
-            if "Token" in decoded_bytes:
+            if "Token" in decoded_bytes and (int(round(time.time() * 1000)) - last_read > 5*1000):
                 match = re.search("Token: (.+)", decoded_bytes, re.IGNORECASE)
                 token_name = match.group(1)
                 token_name = token_name.replace('\r', '')
-                execute_event(device_name, token_name)
+                execute_event(device_name, token_name, id)
+                last_read = int(round(time.time() * 1000))
         except Exception as e:
-            print(e)
+            print("Exception reading:", str(e))
 
-
-for d in devices:
-    device_serial = serial.Serial(d)
-    thread = threading.Thread(target=read_from_port, args=(device_serial, d, ))
-    thread.start()
--- arduino_driver/websocket_wrapper.py
+++ arduino_driver/websocket_wrapper.py
@@ -1,15 +1,18 @@
 import websocket
 import time
 
+
 class WebSocket:
 
     def __init__(self, ws_address):
+        self.ws_address = ws_address
         self.nano_poker_ws = websocket.WebSocket()
         self.nano_poker_ws.connect(ws_address, on_connect=self.on_connect, on_message=self.on_message, on_error=self.on_error, on_close=self.on_close)
         self.queue = []
+        self.last_time = 0
 
     def on_connect(self, ws, message):
-        print(message)
+       print(message)
 
     def on_message(self, ws, message):
         print(message)
@@ -21,13 +24,25 @@ class WebSocket:
         print("### closed ws ###")
 
     def send(self, data):
-        already_seen = [item for item in self.queue if item[0] == data]
-        if already_seen:
-            if (time.time() - already_seen[0][1]) < 5:
-                return
-            else:
-                self.queue.remove(already_seen[0])
 
-        logger.info(data)
-        self.queue.append((data, time.time()))
-        self.nano_poker_ws.send(data)
+        if time.time() - self.last_time < 5:
+            return
+
+        i = 0
+        while True:
+            try:
+                print(data)
+                self.last_time = time.time()
+                self.queue.append((data, time.time()))
+                self.nano_poker_ws.send(data)
+                return
+            except:
+                print("Reconnecting socket...")
+                self.nano_poker_ws = websocket.WebSocket()
+                self.nano_poker_ws.connect(self.ws_address, on_connect=self.on_connect, on_message=self.on_message,
+                                           on_error=self.on_error, on_close=self.on_close)
+                i += 1
+                if i > 5:
+                    raise
+
+
