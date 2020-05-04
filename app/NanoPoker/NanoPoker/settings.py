907a60878870733737d569fac609e5360dcb4702
silverstar194 <maloney4@wisc.edu>
2020-05-04 14:15:38 -0700

    added changes

--- app/NanoPoker/NanoPoker/settings.py
+++ app/NanoPoker/NanoPoker/settings.py
@@ -27,7 +27,7 @@ NANOTOKEN_ENDPOINT = os.environ['ST__NANOTOKEN_ENDPOINT']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
